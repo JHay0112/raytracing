68ee22834494e09c04bce12a72e74932ba14a876
Jordan Hay <jordanhay0112@gmail.com>
2021-11-27 16:56:58 +1300

    vec3 definition

---
+++ vec3.h
@@ -0,0 +1,130 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    /*
+        3 Dimensional Vector
+    */
+    public:
+        // Initalisation
+
+        vec3() : e{0, 0, 0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        // Aliases
+
+        double x() const {return e[0];}
+        double y() const {return e[1];}
+        double z() const {return e[2];}
+
+        // Operators
+
+        // Negative
+        vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);}
+        // Indexing
+        double operator[](int i) const {return e[i];}
+        double& operator[](int i) {return e[i];}
+
+        // Arithmetic Operators
+
+        // Addition
+        vec3& operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        // Scaling
+
+        // Multiplication
+        vec3& operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        // Division
+        vec3& operator/=(const double t) {
+            return *this *= 1/t;
+        }
+
+        // Methods
+        double length() const {
+            return sqrt(length_squared());
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+    public:
+        double e[3];
+};
+
+// Type Aliases
+using point3 = vec3;
+using color = vec3;
+
+#endif
+
+// Utility Functions for vec3
+
+// Print behaviour
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << " " << v.e[1] << " " << v.e[2];
+}
+
+// Addition
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+// Subtraction
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+// Piecewise Multiplication
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+// Factor Scaling
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+// (Reversed) Factor Scaling
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+// Division
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
+}
+
+// Dot Product
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+// Cross Product
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+// Unit Vector
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
