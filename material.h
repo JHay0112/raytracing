baefcb52c12985ae6b8aa6312b1b67b33135a8b8
Jordan Hay <jordanhay0112@gmail.com>
2021-11-30 13:03:56 +1300

    Fuzzy balls

--- material.h
+++ material.h
@@ -52,7 +52,7 @@ class metal : public material {
     */
     public:
         // Initialisation
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         // Scattering
         virtual bool scatter(
@@ -61,12 +61,14 @@ class metal : public material {
             // Calculate the reflected direction
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
             // Create a ray from it
-            scattered = ray(rec.p, reflected);
+            // Include a bit of random diffusion based on fuzz
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
     public:
         color albedo;
+        double fuzz;
 };
 
 #endif
