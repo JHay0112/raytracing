08c94608e4c20dfae61c2b59686fc573f19429ef
Jordan Hay <jordanhay0112@gmail.com>
2021-12-04 20:11:36 +1300

    Go back to 400

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main() {
 
     // Image Parameters
     const auto aspect_ratio = 16.0/9.0;
-    const int image_width = 1920;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
