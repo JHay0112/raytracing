340f16037a6698b7021baff5afef5d1f77b04467
Jordan Hay <jordanhay0112@gmail.com>
2021-12-05 17:37:14 +1300

    Complete vectors

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@
 
 // Vectors
 mod vec3;
-pub use crate::vec3::Vec3;
+pub use crate::vec3::{Vec3, Color, Point3};
 
 // Main
 
 fn main() {
-    let mut v = Vec3::new(1.0, 2.0, 3.0);
+    let v = Vec3::new(1.0, 2.0, 3.0);
     let u = Vec3::new(0.0, 1.0, 0.0);
-    v[0] = 2.0;
-    println!("{}", v[0]);
     println!("{}", v + u);
+    println!("{}", vec3::dot(u, v));
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 /// 3D Vectors + Associated Functions
 /// 
 /// Author: Jordan Hay
@@ -19,8 +21,9 @@ use std::fmt;
 /// v[0] = 9.0;
 /// assert_eq!(9.0, v[0]);
 /// ```
+#[derive(Copy, Clone)]
 pub struct Vec3 {
-    pub e: [f32; 3]
+    e: [f32; 3]
 }
 
 /// Standard Functions for Vec3
@@ -30,13 +33,25 @@ impl Vec3 {
         return Self{e: [x, y, z]};
     }
 
-    // Vector Component Aliases
-    pub fn x(self) -> f32 {return self.e[0];}
-    pub fn y(self) -> f32 {return self.e[1];}
-    pub fn z(self) -> f32 {return self.e[2];}
-    pub fn a(self) -> f32 {return self.e[0];}
-    pub fn b(self) -> f32 {return self.e[1];}
-    pub fn c(self) -> f32 {return self.e[2];}
+    // Vector Cartesian Component Aliases
+    pub const fn x(self) -> f32 {return self.e[0];}
+    pub const fn y(self) -> f32 {return self.e[1];}
+    pub const fn z(self) -> f32 {return self.e[2];}
+    // Vector Alphabetical Component Aliases
+    pub const fn a(self) -> f32 {return self.e[0];}
+    pub const fn b(self) -> f32 {return self.e[1];}
+    pub const fn c(self) -> f32 {return self.e[2];}
+
+    // Vector Attributes
+    /// Magnitude of vector
+    fn magnitude(self) -> f32 {
+        return (self[0].powf(2.0) + self[1].powf(2.0) + self[2].powf(2.0)).sqrt();
+    }
+
+    /// Length of vector (alias of Vec3::magnitude())
+    fn length(self) -> f32 {
+        return self.magnitude();
+    }
 }
 
 /// Default Vector
@@ -57,7 +72,39 @@ impl fmt::Display for Vec3 {
 impl ops::Add for Vec3 {
     type Output = Self;
     fn add(self, other: Self) -> Self::Output {
-        return Vec3::new(self[0] + other[0], self[1] + other[1], self[2] + other[2]);
+        return Self::new(self[0] + other[0], self[1] + other[1], self[2] + other[2]);
+    }
+}
+
+/// Negative Vectors
+impl ops::Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        return Self::new(-self[0], -self[1], -self[2]);
+    }
+}
+
+/// Vector Subtraction
+impl ops::Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other: Vec3) -> Self::Output {
+        return Self::new(self[0] - other[0], self[1] - other[1], self[2] - other[2]);
+    }
+}
+
+/// Vector Scalar Multiplication
+impl ops::Mul<f32> for Vec3 {
+    type Output = Self;
+    fn mul(self, scalar: f32) -> Self::Output {
+        return Self::new(self[0] * scalar, self[1] * scalar, self[2] * scalar);
+    }
+}
+
+/// Vector Scalar Division
+impl ops::Div<f32> for Vec3 {
+    type Output = Self;
+    fn div(self, scalar: f32) -> Self::Output {
+        return Self::new(self[0] / scalar, self[1] / scalar, self[2] / scalar);
     }
 }
 
@@ -69,9 +116,32 @@ impl ops::Index<usize> for Vec3 {
     }
 }
 
-/// Mutate Vector Components with Index
-impl ops::IndexMut<usize> for Vec3 {
-    fn index_mut(&mut self, i: usize) -> &mut f32 {
-        return &mut self.e[i];
-    }
+// Functions
+
+/// Vector Dot Product
+pub fn dot(u: Vec3, v: Vec3) -> f32 {
+    return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
 }
+
+/// Vector Cross Product
+pub fn cross(u: Vec3, v: Vec3) -> Vec3 {
+    return Vec3::new(u[1] * v[2] - u[2] * v[1],
+                     u[2] * v[0] - u[0] * v[2],
+                     u[0] * v[1] - u[1] * v[0]);
+}
+
+/// Normalize Vector
+pub fn normalize(u: Vec3) -> Vec3 {
+    let magnitude = u.magnitude();
+    return Vec3::new(u[0]/magnitude, u[1]/magnitude, u[2]/magnitude);
+}
+
+/// Angle between two vectors
+pub fn angle_between(u: Vec3, v: Vec3) -> f32 {
+    return (dot(u, v)/(u.magnitude() * v.magnitude())).acos();
+}
+
+// Aliases
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
